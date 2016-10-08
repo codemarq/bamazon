899900a23f8ff82a953b7200847ddebb7d311d03
John-Mike <jmmarquardt@gmail.com>
2016-10-08 03:50:33 -0400

    crud functions in js

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,5 +1,5 @@
 //  dependencies
-var sql = require('mysql');
+var mysql = require('mysql');
 var Table = require('cli-table');
 var inquirer = require('inquirer');
 
@@ -9,5 +9,67 @@ var connection = mysql.createConnection({
 	port: 3306,
 	user: 'root',
 	password: '50Wookie$',
-	database: 'Bamazon'
+	database: 'bamazon'
 });
+
+
+
+// connect to DB
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    displayTable();
+})
+
+function displayTable () {
+	// call table constructor
+	var table = new Table ({
+		head: ['ID', 'Product', 'Department', 'Price', 'Quantity'],
+		colWidths: [5, 20, 15, 10, 10]
+	});
+
+	// read all data from products table
+	connection.query('SELECT * FROM Products', [{}], function(error, result) {
+		if (error) throw error;
+
+		for (var i = 0; i < result.length; i++) {
+			// console.log("song: " + result[i].title);
+			table.push([result[i].ItemID, result[i].ProductName, result[i].DepartmentName, result[i].Price, result[i].StockQuantity]);
+		}
+		console.log(table.toString());
+	});
+}
+
+// update db function
+function updateDB () {
+	connection.query("UPDATE Products SET ? WHERE ?", [{
+    	quantity: 100
+	}, {
+    	flavor: "Rocky Road"
+	}], 	function(err, res) {});
+};
+
+// delete from db
+function deleteItem () {
+	connection.query("DELETE FROM Products WHERE ?", {
+	    flavor: "strawberry"
+	}, function(err, res) {});
+};
+
+// insert into db
+function insertItem (product, department, price, quantity) {
+	connection.query("INSERT INTO Products SET ?", {
+	    ProductName: product,
+	    DepartmentName: department,
+	    Price: price,
+	    StockQuantity: quantity
+	}, function(err, res) {});
+};
+
+// select from db
+function select () {
+	connection.query('SELECT * FROM Products', function(err, res) {
+	    if (err) throw err;
+	    console.log(res);
+	})
+};
